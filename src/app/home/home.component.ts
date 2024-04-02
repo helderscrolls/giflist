329db51f3268de407dccbe0ea6c45cac82ad6063
helderscrolls <nortehelder@gmail.com>
2024-04-02 23:00:54 +0200

    feat: add pagination and infinite scroll

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,18 @@
 import { Component, inject } from '@angular/core';
+import { InfiniteScrollModule } from 'ngx-infinite-scroll';
 import { RedditService } from '../shared/data-access/reddit.service';
 import { GifListComponent } from './ui/gif-list.component';
 
 @Component({
   standalone: true,
   selector: 'gl-home',
-  template: `<gl-gif-list [gifs]="redditService.gifs()"></gl-gif-list>`,
-  imports: [GifListComponent],
+  template: `<gl-gif-list
+    [gifs]="redditService.gifs()"
+    infiniteScroll
+    (scrolled)="redditService.pagination$.next(redditService.lastKnownGif())"
+    class="grid-container"
+  ></gl-gif-list>`,
+  imports: [GifListComponent, InfiniteScrollModule],
 })
 export default class HomeComponent {
   redditService = inject(RedditService);
