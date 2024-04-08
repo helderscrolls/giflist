bd7b78d5c1a6c1d771504d2ac15f1752367fb584
helderscrolls <nortehelder@gmail.com>
2024-04-08 16:08:07 +0200

    refactor: migrated state into ngxtension by using connect()

--- src/app/shared/data-access/reddit.service.ts
+++ src/app/shared/data-access/reddit.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable, computed, inject, signal } from '@angular/core';
-import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { FormControl } from '@angular/forms';
+import { connect } from 'ngxtension/connect';
 import {
   EMPTY,
   Subject,
@@ -11,6 +11,7 @@ import {
   distinctUntilChanged,
   expand,
   map,
+  merge,
   startWith,
   switchMap,
 } from 'rxjs';
@@ -87,32 +88,25 @@ export class RedditService {
 
   constructor() {
     // reducers
-    this.subredditChanged$.pipe(takeUntilDestroyed()).subscribe(() => {
-      this.state.update((state) => ({
-        ...state,
-        loading: true,
-        gifs: [],
-        lastKnownGif: null,
-        error: null,
-      }));
-    });
+    const nextState$ = merge(
+      this.subredditChanged$.pipe(
+        map(() => ({
+          loading: true,
+          gifs: [],
+          lastKnownGif: null,
+          error: null,
+        }))
+      ),
+      this.error$.pipe(map((error) => ({ error, loading: false })))
+    );
 
-    this.gifsLoaded$.pipe(takeUntilDestroyed()).subscribe((response) =>
-      this.state.update((state) => ({
-        ...state,
+    connect(this.state)
+      .with(nextState$)
+      .with(this.gifsLoaded$, (state, response) => ({
         gifs: [...state.gifs, ...response.gifs],
         loading: false,
         lastKnownGif: response.lastKnownGif,
-      }))
-    );
-
-    this.error$.pipe(takeUntilDestroyed()).subscribe((error) => {
-      this.state.update((state) => ({
-        ...state,
-        loading: false,
-        error,
       }));
-    });
   }
 
   private fetchFromReddit(
